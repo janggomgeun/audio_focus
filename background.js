65c30616006338b1ae87b049adc4cf6efdc4be4a
Woong Geun Jang <janggomgeun@gmail.com>
2019-06-17 21:30:28 +0900

    Refactor to wrap functions with class

--- background.js
+++ background.js
@@ -2,85 +2,90 @@
  * https://developers.chrome.com/extensions/api_index
  */
 
-chrome.runtime.onInstalled.addListener(function(details) {
-  chrome.browserAction.setBadgeText({text:"OFF"})
-  chrome.browserAction.setBadgeBackgroundColor({color:"#ff4040"})
-})
+ class AudioFocus {
+   constructor() {
+     this.active = false
+     this.activeTabId = -1
+     this.onFocus = false
 
-/*
- * Status of tabs can be as following
- */
+     let self = this
+     chrome.runtime.onInstalled.addListener(function(details) {})
+     chrome.browserAction.setBadgeText({text:"OFF"})
+     chrome.browserAction.setBadgeBackgroundColor({color:"#ff4040"})
 
-var onFocus = false
-function focus(tabId) {
-  console.log('This extension will focus on the tab with the id ' + tabId);
-  chrome.tabs.query({url: ["http://*/*", "https://*/*"]}, function(tabs) {
-    for (var i=0; i<tabs.length; ++i) {
-      if (tabs[i].id === tabId) {
-        chrome.tabs.sendMessage(tabs[i].id, {
-          what: "focus"
-        });
-      } else {
-        chrome.tabs.sendMessage(tabs[i].id, {
-          what: "focusout"
-        });
-      }
-    }
-    onFocus = true
-  });
-}
+     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+       console.log('active tab id is initialized with the value ' + tabs[0].id);
+       self.activeTabId = tabs[0].id
+     })
 
-function back() {
-  chrome.tabs.query({}, function(tabs) {
-    for (var i=0; i<tabs.length; ++i) {
-      chrome.tabs.sendMessage(tabs[i].id, {
-        what: "back"
-      });
-    }
-    onFocus = false
-  });
-}
+     chrome.browserAction.onClicked.addListener(function(tab) {
+       console.log('browser action clicked')
+       self.active = !self.active
+       setActive(self.active)
+     })
 
-var active = false
-function setActive(active) {
-  if (active) {
-    focus(activeTabId)
-  } else {
-    back()
-  }
-}
+     chrome.tabs.onActivated.addListener(function(activeInfo) {
+       console.log('active tab has just changed to the ' + activeInfo.tabId);
+       self.activeTabId = activeInfo.tabId
+       if (self.active) {
+         console.log('active tab id is ' + self.activeTabId);
+         self.focus(self.activeTabId)
+       }
+     })
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('browser action clicked');
-  active = !active
-  setActive(active)
-})
+     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+       if (changeInfo.url) {
+         chrome.tabs.sendMessage(tabId, {
+           what: "update"
+         })
+       }
+     })
 
-var activeTabId
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  console.log('active tab id is initialized with the value ' + tabs[0].id);
-  activeTabId = tabs[0].id
-})
+     chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+       if (tabId === self.activeTabId) {
+         back()
+       }
+     })
+   }
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  console.log('active tab has just changed to the ' + activeInfo.tabId);
-  activeTabId = activeInfo.tabId
-  if (active) {
-    console.log('active tab id is ' + activeTabId);
-    focus(activeTabId)
-  }
-})
+   setActive(active) {
+     if (active) {
+       focus(this.activeTabId)
+     } else {
+       back()
+     }
+   }
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.url) {
-    chrome.tabs.sendMessage(tabId, {
-      what: "update"
-    })
-  }
-})
+   focus(tabId) {
+     console.log('This extension will focus on the tab with the id ' + tabId)
+     let self = this
+     chrome.tabs.query({url: ["http://*/*", "https://*/*"]}, function(tabs) {
+       for (var i=0; i<tabs.length; ++i) {
+         if (tabs[i].id === tabId) {
+           chrome.tabs.sendMessage(tabs[i].id, {
+             what: "focus"
+           })
+         } else {
+           chrome.tabs.sendMessage(tabs[i].id, {
+             what: "focusout"
+           })
+         }
+       }
+       self.onFocus = true
+     })
+   }
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-  if (tabId === activeTabId) {
-    back()
-  }
-})
+   back() {
+     let self = this
+     chrome.tabs.query({}, function(tabs) {
+       for (var i=0; i<tabs.length; ++i) {
+         chrome.tabs.sendMessage(tabs[i].id, {
+           what: "back"
+         });
+       }
+       self.onFocus = false
+     });
+   }
+ }
+
+ new AudioFocus()
