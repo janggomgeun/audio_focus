73cad6718f740b213bd704f96b12c0da1c2e0b9b
janggomgeun <janggomgeun@gmail.com>
2020-09-13 13:42:08 +0900

    Set up Webpack

---
+++ config/webpack.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common.js');
+const PATHS = require('./paths');
+
+// Merge webpack configuration files
+const config = merge(common, {
+  entry: {
+    background: PATHS.src + '/background.js',
+    contentScript: PATHS.src + '/contentScript.js',
+    pageScript: PATHS.src + '/pageScript.js',
+    popup: PATHS.src + '/popup.js',
+  },
+});
+
+module.exports = config;
--- src/background.js
+++ src/background.js
@@ -1,3 +1,10 @@
+'use strict';
+
+// With background scripts you can communicate with popup
+// and contentScript files.
+// For more information on background script,
+// See https://developer.chrome.com/extensions/background_pages
+
 class CommandManager {
   addOnCommandListener(onCommand) {
     chrome.commands.onCommand.addListener(onCommand)
