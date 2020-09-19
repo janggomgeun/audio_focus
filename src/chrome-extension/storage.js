94bd224e311329a7bbb1844326b79aa6623bc9b8
janggomgeun <janggomgeun@gmail.com>
2020-09-19 21:23:31 +0900

    Create a wrapper class for chrome.storage

--- src/chrome-extension/storage.js
+++ src/chrome-extension/storage.js
@@ -1,23 +1,37 @@
+import { USER_SETTINGS } from "../config"
+
 export class Storage {
   constructor(schema, onChanged) {
     this.schema = schema
     this.onChanged = onChanged
-
-    this.init()
-    this.registerSchema(schema)
-
+    this.data = undefined
   }
 
-  async init() {
-    await this.registerSchema(this.schema)
-    await this.addOnChangedListener(this.onChanged)
+  async load() {
+    const self = this
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.get([USER_SETTINGS], function(result) {
+        resolve()
+      })
+    })
   }
 
   async registerSchema(schema) {
-
+    const self = this
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.set(self.schema, function() {
+        resolve()
+      })
+    })
   }
 
   async addOnChangedListener(onChanged) {
-
+    chrome.storage.onChanged.addListener(function(changes, namespace) {
+      switch(namespace) {
+        case 'sync':
+          onChanged(changes)
+          break
+      }
+    })
   }
 }
